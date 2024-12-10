1864159f55a27eb3efc4451c59d2e6c409e3c1ee
einzig1000 <k024g0089@g.neec.ac.jp>
2024-12-10 16:55:54 +0900

    横山：プルするため

---
+++ MapChip.cpp
@@ -0,0 +1,22 @@
+#include "MapChip.h"
+
+static int GetLTblock()
+{
+	Vector2 PlayerLTpos{};
+	Vector2 PlayerLTpos = ;
+
+
+
+}
+static int GetLBblock()
+{
+
+}
+static int GetRTblock()
+{
+
+}
+static int GetRBblock()
+{
+
+}
---
+++ MapChip.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <Vector2.h>
+
+class MapChipJ
+{
+public:
+	static int GetLTblock();
+	static int GetLBblock();
+	static int GetRTblock();
+	static int GetRBblock();
+
+private:
+
+
+
+};
+
